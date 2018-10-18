4fe49084f30b8048cf9d850210e2696b4d1ea842
Kurt Snieckus <ksnieck@alum.wpi.edu>
2018-10-18 00:08:01 -0700

    we've gone back several steps, the alcohol wasn't at the right level this evening. I miss the space shuttle. Added a disclaimer about how i don't give a fuck

--- main.c
+++ main.c
@@ -1,3 +1,7 @@
+// disclaimer.... i'm drunk
+// i know this isn't lisp
+// i was stupid and implemented it on a base of lists and not pairs
+// so go fuck yourself i can do whatever i want
 #include <stdint.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -22,6 +26,11 @@ typedef struct type {
   void* data;
 } type;
 
+type type_make(typeid id, void* data) {
+  type t = {id, data};
+  return t;
+}
+
 typedef struct list {
   struct list* next;
   struct list* prev;
@@ -32,8 +41,7 @@ list* list_make(list* prev, list* next, typeid t, void* data) {
   list* l = malloc(sizeof(list)); // yolo
   l->prev = prev;
   l->next = next;
-  l->type.id = t;
-  l->type.data = data;
+  l->type = type_make(t, data);
   return l;
 }
 
@@ -55,7 +63,7 @@ string* string_cstr(char* s) {
   return string_make(s, i);
 }
 
-int string_eq(string s1, string s2) {
+int string_eq(string s1, string s2) { // returns 0 if they are equal
   if (s1.l != s2.l) return -1; // not even the same length!
   for (uint32_t i=0;i<s1.l;i++) if (s1.c[i] != s2.c[i]) return -1;
   return 0;
@@ -92,21 +100,21 @@ void outputb(char* s, int l) {
   printf("%.*s", l, s);
 }
 
-list* parse_atom(char* buf, uint32_t len) {
+type parse_atom(char* buf, uint32_t len) {
   //  integer i;
   float r;
   /*  if (sscanf(buf, "%lld", &i) != 0)
     return list_make(NULL, NULL, t_integer, integer_make(i));
     else*/
   if(sscanf(buf, "%f", &r) != 0)
-    return list_make(NULL, NULL, t_real, real_make(r));
+    return type_make(t_real, real_make(r));
   else { // MUST BE A SYMBOL!
     void* s = string_make(buf, len);
-    return list_make(NULL, NULL, t_symbol, s);
+    return type_make(t_symbol, s);
   }
 }
 
-list* parse_string() {
+type parse_string() {
   char a;
   int bufl = 256;
   char buf[bufl+1];
@@ -115,7 +123,7 @@ list* parse_string() {
   while (1) {
     a = input();
     if (a == '"' && buf[bufi] != '\\')
-      return list_make(NULL, NULL, t_string, string_make(buf, bufi));
+      return type_make(t_string, string_make(buf, bufi));
     if (bufi>=bufl) {
       buf[bufl] = '\0';
       printf("PANIC: reading input atom with length > %d characters:\n'%s'\n", bufl, buf);
@@ -128,18 +136,18 @@ list* parse_string() {
     
 list* read();
 list* read() {
-  list* h = list_make(NULL, NULL, t_list, NULL);
-  list* l = h;
   char a;
-  int bufl =256;
+  list* l = list_make(NULL, NULL, t_list, NULL);
+  list* h = l;
+  int bufl = 256;
   char buf[bufl+1];
   int bufi = 0;
   while (1) {
     a = input();
     switch (a) {
     case '(':
-      l->next = list_make(NULL, NULL, t_list, read());
-      l = l->next;
+      l->type.id = t_list;
+      l->type.data = list_make(NULL, NULL, t_list, read());
       break;
     case ' ':
     case '\t':
@@ -148,17 +156,19 @@ list* read() {
     case ')':
       if (bufi > 0) {
 	buf[bufi] = '\0';
-	l->next = parse_atom(buf, bufi);
+	bufi = 0;
+	l->type = parse_atom(buf, bufi);
+	l->next = list_make(NULL, NULL, t_void, NULL);
 	l->next->prev = l;
 	l = l->next;
-	bufi = 0;
       }
       if (a == ')') {
 	return h;
       }
       break;
     case '"':
-      l->next = parse_string();
+      l->type = parse_string();
+      l->next = list_make(NULL, NULL, t_void, NULL);
       l->next->prev = l;
       l = l->next;
       break;
@@ -177,7 +187,6 @@ list* read() {
 void print(list* l) {
   char* buf;
   output('(');
-  l = l->next; // list head is a list type with null data (probably a bad idea)
   while (1) {
     if (l == NULL) {
       output(')');
@@ -218,13 +227,8 @@ void print(list* l) {
 }
 
 void print_test() {
-  list* l;
-  list* h;
-  h = list_make(NULL, NULL, t_list, NULL);
-  l = h;
-  l->next = list_make(NULL, NULL, t_symbol, string_make("+", 1));
-  l->next->prev = l;
-  l = l->next;
+  list* l = list_make(NULL, NULL, t_symbol, string_make("+", 1));;
+  list* h = l;
   l->next = list_make(NULL, NULL, t_real, real_make(3));
   l->next->prev = l;
   l = l->next;
@@ -279,17 +283,22 @@ void procedure_add(char* name, char* help, list* (*call)(list* args)) {
 // not sure this declaration is necessary: procedure* env_find(env* e, string* name);
 procedure* env_find(env* e, string name) {
   list* l = e->procedures;
+  string ln;
+  printf("env_find()\n");
   while (1) {
+    printf("env_find-search: ");
+    ln = ((procedure*)l->type.data)->name;
+    printf("%.*s\n", ln.l, ln.c);
     if (l->type.id != t_procedure) {
-      printf("PANIC: env_find() found a non-procedure (%llu) in an environment\n", l->type.id);
-      exit(-1);
+      printf("WARNING: env_find() found a non-procedure (%llu) in an env, skipping\n",
+	     l->type.id);
     }
-    if (string_eq(name, ((procedure*)l->type.data)->name))
+    if (string_eq(name, ((procedure*)l->type.data)->name)==0)
       return l->type.data;
     if (!l->next && e->parent)
       env_find(e->parent, name);
     else {
-      printf("PANIC: couldn't find procedure named '%.*s' in environments\n", name.l, name.c);
+      printf("PANIC: procedure '%.*s' not found.\n", name.l, name.c);
       exit(-2);
     }
     l = l->next;
@@ -299,7 +308,8 @@ procedure* env_find(env* e, string name) {
 list* basic_add(list* a) { // after eval a better be all int / float
   list* t;
   real acc = 0;
-  do {
+  while (1) {
+    printf("basic_add(): eval(%p)\n", a);
     t = eval(a);
     if (t->type.id != t_real) {
       printf("PANIC: argument to '+' did not eval to real");
@@ -307,7 +317,9 @@ list* basic_add(list* a) { // after eval a better be all int / float
     }
     acc += *((real*)t->type.data);
     // TODO: list_free(t)
-  } while (a->next != NULL);
+    if (a->next) a = a->next;
+    else break;
+  }
   return list_make(NULL, NULL, t_real, real_make(acc)); // this one eats the whole list
 }
 // other procedures could eat only some of the argument list and then release the rest of it
@@ -319,43 +331,41 @@ void env_basic() {
   procedure_add("+", "accumulate 'real' arguments", basic_add);
 }
 
-list* eval(list* h) {
-  list* l = h;
-  list* p = l->prev;
-  list* r = list_make(NULL, NULL, t_list, NULL);
-
-  do {
-    // traverse the whole fackin list
-    switch (l->type.id) {
-    case t_list: // well clearly we gotta EVAL dis lis
-      l->type.data = eval(l->type.data);
-      break;
-    case t_symbol: // fun part
-      p = l->prev;
-      l = env_find(environment, *(string*)l->type.data)->call(l->next);
-      l->prev = p;
-      // ^^ dats the magic ^^
-      break;
-    case t_void:
-      printf("eval() found a t_void?!\n");
-    case t_real:
-    case t_string:
-    case t_macro:
-    case t_procedure:
-      l = l->next;
-    }
-    // when is eval gonna respect (quote ...) ???!?!?! TODO
-  } while (l->next != NULL);
-  return h;
+list* eval(list* l) {
+  if (!l) return l; // hopefully this only happens when we inside a null list
+  printf("eval(l->type.id=%llu, l->type.data=%p)\n", l->type.id, l->type.data);
+  list* a;
+  switch (l->type.id) {
+  case t_list: // well clearly we gotta EVAL dis lis
+    a = eval(l->type.data);
+    a->prev = l->prev;
+    a->next = l->next;
+    l = a;
+  case t_symbol: // fun part
+    a = l->prev;
+    l = env_find(environment, *(string*)l->type.data)->call(l->next);
+    l->prev = a;
+    // ^^ dats the magic ^^
+    break;
+  case t_void:
+    printf("eval() found a t_void?!\n");
+  case t_real:
+  case t_string:
+  case t_macro:
+  case t_procedure:
+    printf("eval() got an atom %p\n", l->next);
+  }
+  return l;
 }
 
 int main(int argc, char** argv) {
+  print_test(); printf("\n");
   int depth = 0;
   char a;
   list* l;
-  printf("(");
+  env_basic();
   l = read();
-  l = eval(l);
+  //  l = eval(l);
   print(l);
   output('\n');
 }
